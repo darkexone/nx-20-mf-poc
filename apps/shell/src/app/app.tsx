f9b7e720a91ffe52b763c6d8e025b86bceb3e06a
Hubert Krzeminski <hubert.krzeminski@epicode.net>
2024-10-29 13:23:10 +0100

    Generate remote1 app

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -4,6 +4,8 @@ import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
 
+const Remote1 = React.lazy(() => import('remote1/Module'));
+
 export function App() {
   return (
     <React.Suspense fallback={null}>
@@ -11,9 +13,13 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/remote1">Remote1</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/remote1" element={<Remote1 />} />
       </Routes>
     </React.Suspense>
   );
