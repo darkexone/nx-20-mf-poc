2dace2dc87e62fe5f686126c5a4b45a36ce342d4
Hubert Krzeminski <hubert.krzeminski@epicode.net>
2024-10-29 13:54:48 +0100

    Render products in remote1

--- apps/remote1/src/app/app.tsx
+++ apps/remote1/src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { Products } from '@react-monorepo/products';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <Products />
       <NxWelcome title="remote1" />
     </div>
   );
