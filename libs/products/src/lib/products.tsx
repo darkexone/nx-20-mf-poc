44a62b2847a71c1427b1101ac7dc2788fff4796f
Hubert Krzeminski <hubert.krzeminski@epicode.net>
2024-10-29 13:54:22 +0100

    Use user context in products

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,9 +1,12 @@
+import { useUser } from '@react-monorepo/context';
 import styles from './products.module.css';
 
 export function Products() {
+  const { user } = useUser();
   return (
     <div className={styles['container']}>
       <h1>Welcome to Products!</h1>
+      <p>User: {user?.name ?? 'null'}</p>
     </div>
   );
 }
