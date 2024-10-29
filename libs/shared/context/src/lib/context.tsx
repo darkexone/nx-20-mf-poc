8262dec153488d22cfd84189c7576b4094866979
Hubert Krzeminski <hubert.krzeminski@epicode.net>
2024-10-29 13:39:08 +0100

    Generate shared context library

---
+++ libs/shared/context/src/lib/context.tsx
@@ -0,0 +1,11 @@
+import styles from './context.module.css';
+
+export function Context() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Context!</h1>
+    </div>
+  );
+}
+
+export default Context;
