1f475c2f01c009aab2125a14f4276714ab27dcfb
Hubert Krzeminski <hubert.krzeminski@epicode.net>
2024-10-29 13:53:11 +0100

    Create user context

--- libs/shared/context/src/index.ts
+++ libs/shared/context/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/context';
+export { useUser } from './lib/contexts/UserContext';
--- libs/shared/context/src/lib/context.tsx
+++ libs/shared/context/src/lib/context.tsx
@@ -1,9 +1,12 @@
+import { PropsWithChildren } from 'react';
 import styles from './context.module.css';
+import { UserProvider } from './contexts/UserContext';
 
-export function Context() {
+export function Context({ children }: PropsWithChildren) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Context!</h1>
+      <UserProvider>{children}</UserProvider>
     </div>
   );
 }
