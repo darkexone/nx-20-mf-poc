83c124e71640cab62d5d36f464a788ee7ffc196a
Hubert Krzeminski <hubert.krzeminski@epicode.net>
2024-10-29 13:53:42 +0100

    Use user context in app

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -3,8 +3,31 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
+import { Context, useUser } from '@react-monorepo/context';
 
 const Remote1 = React.lazy(() => import('remote1/Module'));
+
+const UserInfo = () => {
+  const { user } = useUser();
+  return <p>User: {user?.name ?? 'null'}</p>;
+};
+
+const LogUser = () => {
+  const { setUser } = useUser();
+  return (
+    <button
+      onClick={() =>
+        setUser({
+          id: '1',
+          name: 'User 1',
+          email: 'user1@emplo.pl',
+        })
+      }
+    >
+      Log User 1
+    </button>
+  );
+};
 
 export function App() {
   return (
@@ -17,10 +40,14 @@ export function App() {
           <Link to="/remote1">Remote1</Link>
         </li>
       </ul>
-      <Routes>
-        <Route path="/" element={<NxWelcome title="shell" />} />
-        <Route path="/remote1" element={<Remote1 />} />
-      </Routes>
+      <Context>
+        <UserInfo />
+        <LogUser />
+        <Routes>
+          <Route path="/" element={<NxWelcome title="shell" />} />
+          <Route path="/remote1" element={<Remote1 />} />
+        </Routes>
+      </Context>
     </React.Suspense>
   );
 }
